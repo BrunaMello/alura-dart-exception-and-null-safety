31bb29c947c20922f64093494a1e9e7e7b507f9b
brunamello <brunacrespomello@gmail.com>
2022-11-28 02:46:39 +0000

    Initial commit

---
+++ bin/alura_dart_exception_and_null_safety.dart
@@ -0,0 +1,5 @@
+import 'package:alura_dart_exception_and_null_safety/alura_dart_exception_and_null_safety.dart' as alura_dart_exception_and_null_safety;
+
+void main(List<String> arguments) {
+  print('Hello world: ${alura_dart_exception_and_null_safety.calculate()}!');
+}
---
+++ test/alura_dart_exception_and_null_safety_test.dart
@@ -0,0 +1,8 @@
+import 'package:alura_dart_exception_and_null_safety/alura_dart_exception_and_null_safety.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
