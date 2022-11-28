27f61f9fe4f5adc6c10e24ca294934b6c65ca8c6
brunamello <brunacrespomello@gmail.com>
2022-11-28 04:27:07 +0000

    2 - Handling Exceptions

--- bin/alura_dart_exception_and_null_safety.dart
+++ bin/alura_dart_exception_and_null_safety.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 void main() {
   print("Started Main");
   functionOne();
@@ -6,14 +8,35 @@ void main() {
 
 void functionOne() {
   print("Started F01");
-  functionTwo();
+  // try { functionTwo();
+  // } on FormatException catch (e) {
+  //   print("Was captured inside the FunctionOne");
+  //   print(e.message);
+  //   print(e.source);
+  //   print(e.toString());
+  // } on IOException catch (e) {
+  //   print("IOException was captured inside the FunctionOne");
+  //   print(e.toString());
+  // } on Exception catch (e) {
+  //   print(e.toString());
+  // }
+
+  try { functionTwo();
+  } catch (exception, stackTrace) {
+    print(exception);
+    print(stackTrace);
+    rethrow;
+  } finally {
+    print("Finally");
+  }
   print("Finished F01");
 }
 
 void functionTwo() {
   print("Started F02");
-  for (var i = 0; i < 5; i++){
+  for (var i = 1; i < 5; i++){
     print("F02: $i");
+    double amount = double.parse("Not a number");
   }
   print("Finished F02");
 }
